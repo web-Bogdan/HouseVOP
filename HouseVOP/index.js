241fa09f1fe5df9addb128c6618619e835135920
web-Bogdan <madare78@mail.ru>
2022-01-27 20:33:29 +0300

    version 1.0.0

---
+++ HouseVOP/index.js
@@ -0,0 +1,9 @@
+const close = document.querySelector('.menu__burger');
+const menuWrapp = document.querySelector('.menu__wrapp');
+
+
+close.addEventListener('click', e => {
+    document.body.classList.toggle('lock');
+    menuWrapp.classList.toggle('active');
+    close.classList.toggle('active');
+})
